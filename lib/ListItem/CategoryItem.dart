f71e3d3856e4b6f939e661eb263785775ff89088
Tonye Greene <ellysmoore@gmail.com>
2020-05-07 02:24:59 +0100

    Commit

---
+++ lib/ListItem/CategoryItem.dart
@@ -0,0 +1,125 @@
+class categoryItem{
+  final String image, title, Salary, Rating, sale;
+
+  categoryItem({
+    this.image,
+    this.title,
+    this.Salary,
+    this.Rating,
+    this.sale
+  });
+}
+
+List<categoryItem> itemDiscount =[
+ categoryItem(
+    image: "assets/imgCategoryMan/Man6.jpg",
+    title: "AA Cleaning",
+    Salary: "\$ 10",
+    Rating: "4.8",
+    sale: "923 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man2.jpg",
+    title: "Autograph Salon",
+    Salary: "\$ 200",
+    Rating: "4.2",
+    sale: "892 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man3.jpg",
+    title: "James Mechanics",
+    Salary: "\$ 4",
+    Rating: "4.7",
+    sale: "1422 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man4.jpg",
+    title: "Ikeja Electric",
+    Salary: "\$ 15",
+    Rating: "4.4",
+    sale: "531 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man5.jpg",
+    title: "Abeke Welder",
+    Salary: "\$ 20",
+    Rating: "4.5",
+    sale: "130 Sale",
+  ),
+];
+
+List<categoryItem> itemPopularData = [
+  categoryItem(
+    image: "assets/imgCategoryMan/Man6.jpg",
+    title: "AA Cleaning",
+    Salary: "\$ 10",
+    Rating: "4.8",
+    sale: "923 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man5.jpg",
+    title: "Autograph Salon",
+    Salary: "\$ 200",
+    Rating: "4.2",
+    sale: "892 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man4.jpg",
+    title: "James Mechanics",
+    Salary: "\$ 4",
+    Rating: "4.7",
+    sale: "1422 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man3.jpg",
+    title: "Ikeja Electric",
+    Salary: "\$ 15",
+    Rating: "4.4",
+    sale: "531 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man3.jpg",
+    title: "Abeke Welder",
+    Salary: "\$ 20",
+    Rating: "4.5",
+    sale: "130 Sale",
+  ),
+];
+
+List<categoryItem> newItems = [
+  categoryItem(
+    image: "assets/imgCategoryMan/Man6.jpg",
+    title: "AA Cleaning",
+    Salary: "\$ 10",
+    Rating: "4.8",
+    sale: "923 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man5.jpg",
+    title: "Autograph Salon",
+    Salary: "\$ 200",
+    Rating: "4.2",
+    sale: "892 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man4.jpg",
+    title: "James Mechanics",
+    Salary: "\$ 4",
+    Rating: "4.7",
+    sale: "1422 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man3.jpg",
+    title: "Ikeja Electric",
+    Salary: "\$ 15",
+    Rating: "4.4",
+    sale: "531 Sale",
+  ),
+  categoryItem(
+    image: "assets/imgCategoryMan/Man3.jpg",
+    title: "Abeke Welder",
+    Salary: "\$ 20",
+    Rating: "4.5",
+    sale: "130 Sale",
+  ),
+];
---
+++ lib/ListItem/HomeGridItemRecomended.dart
@@ -0,0 +1,101 @@
+class GridItem {
+  final String id;
+  final String img;
+  final String title;
+  final String price;
+  final String rattingValue;
+  final String itemSale;
+  final String description;
+
+  GridItem(
+      {this.id,
+        this.img,
+        this.title,
+        this.price,
+        this.rattingValue,
+        this.itemSale,
+        this.description});
+}
+
+List<GridItem> gridItemArray = [
+  GridItem(
+    id: "1",
+    img: "assets/imgItem/fashion1.jpg",
+    title: "Electrician shop",
+    price: "\$50",
+    itemSale: "932 Services",
+    rattingValue: "4.8",
+    description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.....",
+  ),
+  GridItem(
+    id: "2",
+    img: "assets/imgItem/acesoris1.jpg",
+    title: "Welder Shop",
+    price: "\$200",
+    itemSale: "892 Services",
+    rattingValue: "4.2",
+    description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.....",
+  ),
+  GridItem(
+    id: "3",
+    img: "assets/imgItem/beauty1.jpg",
+    title: "Tbeauty",
+    price: "Starting \$14",
+    itemSale: "1422 Services",
+    rattingValue: "4.7",
+    description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.....",
+  ),
+  GridItem(
+    id: "4",
+    img: "assets/imgItem/man1.jpg",
+    title: "AA Cleaning",
+    price: "Starting \$15",
+    itemSale: "523 Services",
+    rattingValue: "4.4",
+    description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.....",
+  ),
+  GridItem(
+    id: "5",
+    img: "assets/imgItem/girl1.jpg",
+    title: "Style by Aduke",
+    price: "Starting \$20",
+    itemSale: "130 Services",
+    rattingValue: "4.5",
+    description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.....",
+  ),
+  GridItem(
+    id: "6",
+    img: "assets/imgItem/kids1.jpg",
+    title: "Mechanic Shop",
+    price: "Starting \$33",
+    itemSale: "110 Services",
+    rattingValue: "4.8",
+    description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.....",
+  ),
+  GridItem(
+    id: "7",
+    img: "assets/imgItem/shoes1.jpg",
+    title: "Autograph Salon",
+    price: "Starting \$45",
+    itemSale: "654 Services",
+    rattingValue: "4.1",
+    description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.....",
+  ),
+  GridItem(
+    id: "8",
+    img: "assets/imgItem/shoes2.jpg",
+    title: "Studio24",
+    price: "Starting \$100",
+    itemSale: "1542 Services",
+    rattingValue: "4.1",
+    description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.....",
+  ),
+];
